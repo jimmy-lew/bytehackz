1eed718cc68fd9b14e2ee87c6428a04dc5014f04
jimmy <s10219505@connect.np.edu.sg>
2022-10-21 17:02:42 +0800

    refactor: implement tailwind & daisyui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'astro-purple': '#7c3aed',
+				'astro-pink': '#da62c4'
+			},
+			backgroundSize: {
+				'400': '400%'
+			},
+			backgroundImage: {
+				'astro-gradient': 'linear-gradient(45deg, #7c3aed, #da62c4 30%, white 60%)'
+			},
+			transitionTimingFunction: {
+				'astro': 'cubic-bezier(0.22, 1, 0.36, 1)'
+			}
+		},
 	},
 	plugins: [
 		require("daisyui")
