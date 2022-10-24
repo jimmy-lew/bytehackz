b91f7845875aeb0e1c319e5a70d1555bd74a633a
jimmy <s10219505@connect.np.edu.sg>
2022-10-24 14:32:44 +0800

    feat: update link card animation trigger

--- components/LinkCard.vue
+++ components/LinkCard.vue
@@ -6,7 +6,7 @@
 </script>
 
 <template>
-    <li class="card card-compact bg-white rounded-lg shadow-md transition-all ease-astro duration-300 hover:scale-105">
+    <li class="card card-compact bg-white rounded-lg shadow-md transition-all ease-astro duration-300 hover:scale-105 focus:scale-105">
         <a :href="href" class="card-body text-black">
             <div class="card-actions justify-end">
                 <span class="text-gray-400">
