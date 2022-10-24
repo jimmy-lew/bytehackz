a30d4c221febf67d839b0ef11385429979e60a78
jimmy <s10219505@connect.np.edu.sg>
2022-10-24 13:51:02 +0800

    build: add vueuse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    modules: [
+      '@vueuse/nuxt',
+    ],
     css: ["@/assets/css/base.css"],
     build: {
       postcss: {
