339108475bcb394e70f221ed7ef3da8981593816
jimmy <s10219505@connect.np.edu.sg>
2022-10-25 15:02:49 +0800

    feat: update home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,69 +1,48 @@
-<script setup>
-</script>
-
 <template>
-    <main class="m-auto p-6 max-w-5xl">
-        <div class="flex w-full items-center mb-8">
-            <div class="inline-flex w-1/2 justify-start">
-                <h1 class="whitespace-pre-line text-2xl">Hello!
-                    What would you like to do today?</h1>
-            </div>
-            <div class="inline-flex w-1/2 justify-end h-fit items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 21l5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 016-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 01-3.827-5.802" />
-                </svg>
-                <div class="divider divider-horizontal"></div>		
-                <div class="flex flex-col items-center text-xs">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
-                    </svg>		
-                    <p class="text-gray-400">
-                        Exit		
-                    </p>		
-                </div>	
-            </div>
-        </div>
-        <div class="alert alert-warning shadow-lg rounded-md mb-4">
-            <div>
-                <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
-                <span>Warning: This could be a scam!</span>
-            </div>
-        </div>
-        <h2 class="mb-6 font-semibold text-lg">Non Cash Services</h2>
-        <ul role="list" class="grid grid-cols-3 gap-4 p-0 mb-6">
-            <LinkCard :title="'Set up quick \n transaction'" :href="'/transaction'">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12" />
+    <div class="w-5/8 p-12 shadow-right-md">
+        <h1 class="text-3xl whitespace-pre-line mt-8 mb-16 h-10">
+            Hello!<br>
+            What would you like to do today?
+        </h1>
+        <h2 class="font-semibold text-lg mb-8">Get Cash</h2>
+        <ul class="grid grid-cols-2 gap-4 mb-4">
+            <button class="btn btn-lg text-2xl leading-0 py-8 bg-gradient-to-tr from-gray-700 to-black">$50</button>
+            <button class="btn btn-lg text-2xl leading-0 py-8 bg-gradient-to-tr from-gray-700 to-black">$80</button>
+            <button class="btn btn-lg text-2xl leading-0 py-8 bg-gradient-to-tr from-gray-700 to-black">$100</button>
+            <button class="btn btn-lg text-2xl leading-0 py-8 bg-gradient-to-tr from-gray-700 to-black">$500</button>
+        </ul>
+        <button class="btn btn-lg text-lg leading-0 py-8 bg-gradient-to-tr from-gray-700 to-black w-full">Other amounts</button>
+    </div>
+    <div class="w-3/8 p-8">
+        <div class="inline-flex w-full justify-end h-fit items-center mb-28 mr-4">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 21l5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 016-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 01-3.827-5.802" />
             </svg>
+            <div class="divider divider-horizontal"></div>		
+            <div class="flex flex-col items-center text-xs">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
+                </svg>		
+                <p class="text-gray-400">
+                    Exit		
+                </p>		
+            </div>	
+        </div>
+        <h2 class="font-semibold text-lg mb-8">Non cash services</h2>
+        <ul class="grid grid-cols-2 gap-4 mb-4">
+            <LinkCard :title="'Activate \n Card'" :href="''">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5z" />
+                </svg>
             </LinkCard>
             <LinkCard :title="'Balance \n Enquiry'" :href="''">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    </svg>
-            </LinkCard>
-            <LinkCard :title="'Bill \n Payment'" :href="''">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
-                    </svg>	
-            </LinkCard>
-            <LinkCard :title="'Transfer \n Funds'" :href="'/transfer'">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 21L3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5" />
-                    </svg>
-            </LinkCard>
-            <LinkCard :title="'CPF \n Services'" :href="''">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25a3 3 0 013 3m3 0a6 6 0 01-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1121.75 8.25z" />
-                    </svg>	
-            </LinkCard>
-            <LinkCard :title="'Investment \n Services'" :href="''">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" />
-                    </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
             </LinkCard>
         </ul>
-        <Modal>
-            Test modal
-        </Modal>
-    </main>
+        <a href="/services" class="link link-hover link-accent inline-flex w-full justify-end">
+            More services
+        </a>
+    </div>
 </template>
