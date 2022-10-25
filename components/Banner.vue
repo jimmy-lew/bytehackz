18c66caa26ccf0dd7721363efd1a2760c5b60131
jimmy <s10219505@connect.np.edu.sg>
2022-10-25 15:02:34 +0800

    feat: add banner component

---
+++ components/Banner.vue
@@ -0,0 +1,18 @@
+<script setup>
+    const isOpen = ref(true);
+
+    const close = () => {
+        isOpen.value = false;
+    }
+</script>
+
+<template>
+    <div v-if="isOpen" class="alert alert-warning shadow-lg rounded-md mb-4">
+        <div class="w-1/2">
+            <slot />
+        </div>
+        <div class="w-1/2 inline-flex justify-end">
+            <button @click="close" class="btn btn-sm btn-circle btn-ghost">✕</button>
+        </div>
+    </div>
+</template>
