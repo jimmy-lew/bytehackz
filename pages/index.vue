1cbe07b5a56c941ed6581ffac74aad8c5b398c12
jimmy <s10219505@connect.np.edu.sg>
2022-10-31 12:01:32 +0800

    refactor: replace pages with icon component

--- pages/index.vue
+++ pages/index.vue
@@ -15,14 +15,10 @@
     </div>
     <div class="w-3/8 p-8">
         <div class="inline-flex w-full justify-end h-fit items-center mb-28 mr-4">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 21l5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 016-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 01-3.827-5.802" />
-            </svg>
+            <Icon size="24" name="heroicons:language-20-solid"/> 
             <div class="divider divider-horizontal"></div>		
             <div class="flex flex-col items-center text-xs">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
-                </svg>		
+                <Icon size="24" name="heroicons:arrow-right-on-rectangle-20-solid"/> 
                 <p class="text-gray-400">
                     Exit		
                 </p>		
@@ -31,14 +27,10 @@
         <h2 class="font-semibold text-lg mb-8">Non cash services</h2>
         <ul class="grid grid-cols-2 gap-4 mb-6">
             <LinkCard :title="'Activate \n Card'" :href="''">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5z" />
-                </svg>
+                <Icon size="32" name="heroicons:credit-card"/>
             </LinkCard>
             <LinkCard :title="'Balance \n Enquiry'" :href="''">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
+                <Icon size="32" name="heroicons:currency-dollar"/>
             </LinkCard>
         </ul>
         <a href="/services" class="link link-hover link-accent inline-flex w-full justify-end">
--- pages/transfer/confirm.vue
+++ pages/transfer/confirm.vue
@@ -21,9 +21,7 @@
                         <h2 class="font-semibold text-xl whitespace-nowrap">S$ {{amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}}</h2>
                     </div>
                     <div class="inline-flex items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 h-8 fill-gray-300">
-                            <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z" clip-rule="evenodd" />
-                        </svg>                  
+                        <Icon size="32" name="heroicons:arrow-right-circle-20-solid" class="stroke-gray-300"/>          
                     </div>
                     <div class="flex flex-col w-1/2 content-end text-end ml-16">
                         <p class="text-gray-400 text-sm mb-2">To</p>
