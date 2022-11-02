685e692043d6aa8e74ae88aeb92d4b3746a861ff
jimmy <s10219505@connect.np.edu.sg>
2022-11-03 01:14:45 +0800

    feat: implement TheCashRadialMenu component

--- pages/index.vue
+++ pages/index.vue
@@ -9,28 +9,29 @@
 			<Icon size="24" name="maki:caution" />
 			<span>Warning: This could be a scam!</span>
 		</Banner>
-		<div class="flex">
-			<div class="flex w-1/2 gap-8">
-				<LinkCard title="Withdrawal" href="/">
-					<Icon size="36" name="fluent:money-hand-24-regular" class="" />
+		<div class="flex gap-8">
+			<LinkCard title="Withdrawal" href="/">
+				<Icon size="36" name="fluent:money-hand-24-regular" class="" />
+			</LinkCard>
+			<ul class="flex flex-col gap-8">
+				<LinkCard title="Payment" href="/">
+					<Icon size="36" name="fa6-solid:file-signature" class="" />
 				</LinkCard>
-				<ul class="flex flex-col gap-8">
-					<LinkCard title="Payment" href="/">
-						<Icon size="36" name="fa6-solid:file-signature" class="" />
-					</LinkCard>
-					<LinkCard title="Transfer" href="/">
-						<Icon size="36" name="heroicons:arrows-right-left-20-solid" />
-					</LinkCard>
-				</ul>
-			</div>
-			<div class="flex w-1/2 gap-8 justify-end">
-				<LinkCard title="More" href="/services">
+				<LinkCard title="Transfer" href="/">
+					<Icon size="36" name="heroicons:arrows-right-left-20-solid" />
+				</LinkCard>
+			</ul>
+			<!-- <div class="flex w-1/2 gap-8">
+			</div> -->
+			<TheCashRadialMenu />
+			<div class="ml-auto">
+				<LinkCard title="More" href="/services" class="h-2/3">
 					<Icon size="32" name="heroicons-solid:dots-horizontal" class="" />
 				</LinkCard>
 			</div>
 		</div>
 	</div>
-	<Modal>
+	<!-- <Modal>
 		Test
-	</Modal>
+	</Modal> -->
 </template>
