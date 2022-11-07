047b01ed04c9dcc351639d0c29f87c3063c5d955
jimmy <s10219505@connect.np.edu.sg>
2022-11-07 23:14:17 +0800

    build: add tailwind

---
+++ dashboard/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		'./assets/**/*.css',
+		'./components/*.{vue,js}',
+		'./components/**/*.{vue,js}',
+		'./layouts/**/*.{vue,js}',
+		'./layouts/*.{vue,js}',
+		'./pages/*.vue',
+		'./pages/**/*.vue',
+		'./plugins/**/*.{js,ts}',
+		'./*.{vue,js,ts}',
+		'./nuxt.config.{js,ts}',
+	],
+	darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
