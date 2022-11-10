abdf69d0c2610579d1227729a6d0aa19f989f101
Jeffrey Zhong Zeng Jie <s10223330@connect.np.edu.sg>
2022-11-10 23:14:11 +0800

    feat: preprocess images

---
+++ sentiment-analysis/TrainEmotionDetector.py
@@ -0,0 +1,23 @@
+from keras.preprocessing.image import ImageDataGenerator
+
+# Initialize image data generator with rescaling
+train_data_gen = ImageDataGenerator(rescale=1./255)
+validation_data_gen = ImageDataGenerator(rescale=1./255)
+
+# Preprocess all test images
+train_generator = train_data_gen.flow_from_directory(
+        'data/train',
+        target_size=(48, 48),
+        batch_size=64,
+        color_mode="grayscale",
+        class_mode='categorical')
+
+# Preprocess all train images
+validation_generator = validation_data_gen.flow_from_directory(
+        'data/test',
+        target_size=(48, 48),
+        batch_size=64,
+        color_mode="grayscale",
+        class_mode='categorical')
+
+
