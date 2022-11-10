68731fad31431e51b90b4ed97c1131a88bb074d2
Jeffrey Zhong Zeng Jie <s10223330@connect.np.edu.sg>
2022-11-10 23:18:57 +0800

    feat: created model structure

--- sentiment-analysis/TrainEmotionDetector.py
+++ sentiment-analysis/TrainEmotionDetector.py
@@ -1,3 +1,8 @@
+# Import required packages
+import cv2
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Flatten
+from tensorflow.keras.optimizers import Adam
 from keras.preprocessing.image import ImageDataGenerator
 
 # Initialize image data generator with rescaling
@@ -20,4 +25,26 @@ validation_generator = validation_data_gen.flow_from_directory(
         color_mode="grayscale",
         class_mode='categorical')
 
+# create model structure
+emotion_model = Sequential()
+
+emotion_model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48, 48, 1)))
+emotion_model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Dropout(0.25))
+
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Dropout(0.25))
+
+emotion_model.add(Flatten())
+emotion_model.add(Dense(1024, activation='relu'))
+emotion_model.add(Dropout(0.5))
+emotion_model.add(Dense(7, activation='softmax'))
+
+cv2.ocl.setUseOpenCL(False)
+
+emotion_model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.0001, decay=1e-6), metrics=['accuracy'])
 
