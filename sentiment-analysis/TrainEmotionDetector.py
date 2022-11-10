48a2f1065f61d1ae7904a04422f5baf00ae09726
Jeffrey Zhong Zeng Jie <s10223330@connect.np.edu.sg>
2022-11-10 23:29:49 +0800

    feat: implemented training of model

--- sentiment-analysis/TrainEmotionDetector.py
+++ sentiment-analysis/TrainEmotionDetector.py
@@ -1,15 +1,15 @@
-# Import required packages
+# import required packages
 import cv2
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Flatten
 from tensorflow.keras.optimizers import Adam
 from keras.preprocessing.image import ImageDataGenerator
 
-# Initialize image data generator with rescaling
+# initialize image data generator with rescaling
 train_data_gen = ImageDataGenerator(rescale=1./255)
 validation_data_gen = ImageDataGenerator(rescale=1./255)
 
-# Preprocess all test images
+# preprocess all test images
 train_generator = train_data_gen.flow_from_directory(
         'data/train',
         target_size=(48, 48),
@@ -17,7 +17,7 @@ train_generator = train_data_gen.flow_from_directory(
         color_mode="grayscale",
         class_mode='categorical')
 
-# Preprocess all train images
+# preprocess all train images
 validation_generator = validation_data_gen.flow_from_directory(
         'data/test',
         target_size=(48, 48),
@@ -48,3 +48,18 @@ cv2.ocl.setUseOpenCL(False)
 
 emotion_model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.0001, decay=1e-6), metrics=['accuracy'])
 
+# train the neural network/model
+emotion_model_info = emotion_model.fit_generator(
+        train_generator,
+        steps_per_epoch=28709 // 64,
+        epochs=48,
+        validation_data=validation_generator,
+        validation_steps=7178 // 64)
+
+# save model structure in jason file
+model_json = emotion_model.to_json()
+with open("emotion_model.json", "w") as json_file:
+    json_file.write(model_json)
+
+# save trained model weight in .h5 file
+emotion_model.save_weights('emotion_model.h5')
