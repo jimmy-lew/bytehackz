c39cb23955c6b6e31132c8d0fe33750aed2a43d9
jimmy <s10219505@connect.np.edu.sg>
2022-11-14 18:15:03 +0800

    fix: move auth.json to public

--- web/server/api/auth/validate.post.ts
+++ web/server/api/auth/validate.post.ts
@@ -3,15 +3,15 @@ import * as fs from 'fs'
 export default defineEventHandler((event) => {
 	const { auth } = getQuery(event)
 
-	if (!fs.existsSync('./tmp/auth.json'))
-		fs.writeFileSync('./tmp/auth.json', JSON.stringify({}, null, '\t'))
+	if (!fs.existsSync('./public/auth.json'))
+		fs.writeFileSync('./public/auth.json', JSON.stringify({}, null, '\t'))
 
-	const rawData = fs.readFileSync('./tmp/auth.json')
+	const rawData = fs.readFileSync('./public/auth.json')
 	const authData = JSON.parse(rawData.toString())
 
 	authData.isValidated = !!(parseInt(auth.toString()))
 
-	fs.writeFileSync('./tmp/auth.json', JSON.stringify(authData, null, '\t'))
+	fs.writeFileSync('./public/auth.json', JSON.stringify(authData, null, '\t'))
 
 	return authData
 })
