633d6783cdf8e042b44b4aa621170fd18619d603
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-11-15 10:14:04 +0800

    feat: add biometrics xcodeproj

---
+++ ios-biometrics/Biometrics/Biometrics/BiometricsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiometricsApp.swift
+//  Biometrics
+//
+//  Created by Jia Chen Yee on 15/11/22.
+//
+
+import SwiftUI
+
+@main
+struct BiometricsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
