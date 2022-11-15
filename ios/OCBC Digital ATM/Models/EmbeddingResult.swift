3c20940d9030611f281492ff83e1604dad089582
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-11-15 17:05:48 +0800

    feat: add similarity score calculation

---
+++ ios/OCBC Digital ATM/Models/EmbeddingResult.swift
@@ -0,0 +1,14 @@
+//
+//  EmbeddingResult.swift
+//  OCBC Digital ATM
+//
+//  Created by Jia Chen Yee on 15/11/22.
+//
+
+import Foundation
+
+struct EmbeddingResult {
+    var reason: String
+    var normalizedDistance: Double
+    var suspiciousness: Double
+}
--- ios/OCBC Digital ATM/View Models/ResponseManager+SentenceEmbedding.swift
+++ ios/OCBC Digital ATM/View Models/ResponseManager+SentenceEmbedding.swift
@@ -7,21 +7,41 @@
 
 import Foundation
 import NaturalLanguage
+import SwiftCSV
 
 extension ResponseManager {
     func calculateScore(with text: String) -> Double {
         if let sentenceEmbedding = NLEmbedding.sentenceEmbedding(for: .english) {
-            let sentence = "This is a sentence."
             
-            if let vector = sentenceEmbedding.vector(for: sentence) {
-                print(vector)
+            let data: CSV<Named> = try! CSV(name: "reason", extension: "csv", bundle: .main)!
+            
+            var updatedData: [EmbeddingResult] = []
+            
+            for row in data.rows {
+                let reason = row["Reason why sending this money"]
+                let suspiciousness = Double(row["Suspiciousness (0-1)"]!)!
+                
+                let normalizedDistance = 1 / (sentenceEmbedding.distance(between: text, and: reason!))
+                
+                print(normalizedDistance * suspiciousness)
+                
+                updatedData.append(EmbeddingResult(reason: reason!,
+                                                   normalizedDistance: normalizedDistance,
+                                                   suspiciousness: suspiciousness))
             }
             
-            let distance = sentenceEmbedding.distance(between: sentence, and: "That is a sentence.")
+            let topCandidates = updatedData.sorted {
+                $0.normalizedDistance > $1.normalizedDistance
+            }[0..<3]
             
-            print(distance.description)
+            if topCandidates[0].normalizedDistance > 1 {
+                return topCandidates[0].suspiciousness
+            } else {
+                return topCandidates.reduce(0) { partialResult, embedding in
+                    partialResult + embedding.normalizedDistance * embedding.suspiciousness
+                } / 3
+            }
         }
-        
         return 0
     }
 }
