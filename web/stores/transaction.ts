d949ac89fa9de4b633d4040df88ed3d339efafc7
jimmy <s10219505@connect.np.edu.sg>
2022-11-15 11:58:09 +0800

    fix: transaction store method names

--- web/stores/transaction.ts
+++ web/stores/transaction.ts
@@ -2,11 +2,11 @@ import { defineStore } from 'pinia'
 
 export const useTransactionStore = defineStore('transaction', () => {
 	const transactionTo = ref('')
-	const settransactionTo = (accNo: string) => transactionTo.value = accNo
+	const setTransactionTo = (accNo: string) => transactionTo.value = accNo
 	const transactionFrom = ref('')
-	const settransactionFrom = (accNo: string) => transactionFrom.value = accNo
+	const setTransactionFrom = (accNo: string) => transactionFrom.value = accNo
 	const transactionAmount = ref(0)
 	const setTransactionAmount = (amount: number) => transactionAmount.value = amount
 
-	return { transactionFrom, transactionTo, settransactionFrom, settransactionTo, transactionAmount, setTransactionAmount }
+	return { transactionFrom, transactionTo, setTransactionFrom, setTransactionTo, transactionAmount, setTransactionAmount }
 })
