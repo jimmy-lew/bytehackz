9bbb1d1a0a35ee57527ee5e918035e8baf157f23
jimmy <s10219505@connect.np.edu.sg>
2022-11-15 12:09:53 +0800

    update session api

--- web/server/api/session.post.ts
+++ web/server/api/session.post.ts
@@ -24,7 +24,7 @@ export default defineEventHandler(async (event) => {
 		isEmergency: false,
 		appScore: 0,
 		overallScore: 0,
-		transactionType: '',
+		transactionID: randomUUID(),
 		timeCreated: new Date(),
 	}
 
