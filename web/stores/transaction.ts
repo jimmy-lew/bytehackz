378310c2e7ab74aa708af980a4647e077eb3bcf1
jimmy <s10219505@connect.np.edu.sg>
2022-11-15 12:08:50 +0800

    style: organize transaction store

--- web/stores/transaction.ts
+++ web/stores/transaction.ts
@@ -1,12 +1,24 @@
 import { defineStore } from 'pinia'
 
 export const useTransactionStore = defineStore('transaction', () => {
+	const transactionID = ref('')
 	const transactionTo = ref('')
-	const setTransactionTo = (accNo: string) => transactionTo.value = accNo
 	const transactionFrom = ref('')
-	const setTransactionFrom = (accNo: string) => transactionFrom.value = accNo
 	const transactionAmount = ref(0)
+
+	const setTransactionID = (id: string) => transactionID.value = id
+	const setTransactionTo = (accNo: string) => transactionTo.value = accNo
+	const setTransactionFrom = (accNo: string) => transactionFrom.value = accNo
 	const setTransactionAmount = (amount: number) => transactionAmount.value = amount
 
-	return { transactionFrom, transactionTo, setTransactionFrom, setTransactionTo, transactionAmount, setTransactionAmount }
+	return {
+		transactionID,
+		transactionTo,
+		transactionFrom,
+		transactionAmount,
+		setTransactionID,
+		setTransactionFrom,
+		setTransactionTo,
+		setTransactionAmount,
+	}
 })
