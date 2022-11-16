101fb59d672d0e2e3702f7d08f39d4e62a529167
jimmy <s10219505@connect.np.edu.sg>
2022-11-17 00:28:33 +0800

    feat: implement fear score route

--- web/server/api/auth/fearscore.post.ts
+++ web/server/api/auth/fearscore.post.ts
@@ -1,9 +1,22 @@
-// import { db } from './lib/firebase'
+import { db } from '../lib/firebase'
 
 export default defineEventHandler(async (event) => {
 	const body = await useBody(event)
 
-	const { featScore } = body
+	const atmID = '000001'
+	const { fearScore } = body
 
-	return body
+	const atmTransactionsSnapshot = await db
+		.collection(`atms/${atmID}/sessions`)
+		.orderBy('timeCreated')
+		.limitToLast(1)
+		.get()
+
+	await atmTransactionsSnapshot.docs.forEach((doc) => {
+		db.collection(`atms/${atmID}/sessions`).doc(doc.id).update({
+			fearScore,
+		})
+	})
+
+	return {}
 })
