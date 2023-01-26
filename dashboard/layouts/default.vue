4d16bdbb34277360e212b394530f9fa0b2812ef4
jimmy <s10219505@connect.np.edu.sg>
2023-01-26 22:46:08 +0800

    feat: test pages

---
+++ dashboard/layouts/default.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+	<div>
+		<NavDashboard />
+		<slot />
+	</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ dashboard/pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+	<div>
+		Test
+	</div>
+</template>
+
+<style scoped>
+
+</style>
