1434d4c41f4aaa149652969ee016586762c39910
jimmy <s10219505@connect.np.edu.sg>
2023-01-26 22:41:16 +0800

    feat: Progress

---
+++ dashboard/components/progress/Progress.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const props = defineProps<{
+	value: number
+	max: number
+}>()
+</script>
+
+<template>
+	<progress class="align-baseline relative w-full overflow-hidden h-2 rounded-full " v-bind="props" />
+</template>
+
+<style scoped>
+progress::-webkit-progress-bar {
+    @apply bg-gray-300
+}
+
+progress::-webkit-progress-value {
+    @apply bg-rose-600 rounded-full
+}
+</style>
