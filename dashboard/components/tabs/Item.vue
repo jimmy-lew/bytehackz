6a5174ae67886080bd3b5a8534a715dbc233d6ea
jimmy <s10219505@connect.np.edu.sg>
2023-01-26 22:32:21 +0800

    feat: Tabs

---
+++ dashboard/components/tabs/Item.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const props = withDefaults(defineProps<{
+	link: string
+}>(), {
+	link: '/',
+})
+
+const { path } = useRoute()
+
+const isActiveRoute = computed(() => path === props.link)
+</script>
+
+<template>
+	<div class="relative inline-flex flex-wrap justify-content text-sm px-4 pt-2 pb-4 border-rose-600" :class="{ 'border-b': isActiveRoute, 'font-bold': isActiveRoute }">
+		<NuxtLink :to="link">
+			<slot />
+		</NuxtLink>
+	</div>
+</template>
+
+<style scoped>
+
+</style>
