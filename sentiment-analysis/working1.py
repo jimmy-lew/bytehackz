7241088098188675372e1c08a7e1172a11d46de9
Jeffrey Zhong Zeng Jie <s10223330@connect.np.edu.sg>
2023-02-01 12:38:58 +0800

    deepface working test

---
+++ sentiment-analysis/working1.py
@@ -0,0 +1,30 @@
+import cv2
+from deepface import DeepFace
+
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret,frame = cap.read()
+    result = DeepFace.analyze(img_path = frame , actions=['emotion'], enforce_detection=False )
+
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray,1.1,4)
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),3)
+
+    emotion = result["dominant_emotion"]
+    
+    txt = str(emotion)
+
+    cv2.putText(frame,txt,(50,50),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),3)
+    cv2.imshow('frame',frame)
+
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
