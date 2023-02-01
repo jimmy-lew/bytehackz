fb4263bc1a6cec2ee518898f359ae24c5e7c30d2
jimmy <s10219505@connect.np.edu.sg>
2023-02-01 19:58:36 +0800

    test

---
+++ dashboard/components/section/Logs.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+withDefaults(defineProps<{
+	title: string
+}>(), {
+	title: 'Logs',
+})
+
+const dateRange_data = ref<DateRangeItem[]>([
+	{
+		_id: 0,
+		date: 'This Week',
+		value: '7d',
+	},
+	{
+		_id: 1,
+		date: 'This Month',
+		value: '30d',
+	},
+])
+
+const dateRange_db = ref(await lyraCreate({
+	_id: 'number',
+	date: 'string',
+}, dateRange_data.value))
+</script>
+
+<template>
+	<div class="w-full border-b dark:border-white/20">
+		<div class="max-w-7xl mx-auto">
+			<h1 class="text-3xl font-bold my-4">
+				{{ title }}
+			</h1>
+			<div class="flex gap-x-4 my-4">
+				<FormSingleSelect
+					class="border dark:border-white/20 rounded p-1"
+					placeholder="This Week"
+					has-search
+					:db="dateRange_db"
+					:data="dateRange_data"
+					:transformer="({ date }) => date"
+				/>
+				<FormSingleSelect
+					class="border dark:border-white/20 rounded p-1"
+					placeholder="This Week"
+					has-search
+					:db="dateRange_db"
+					:data="dateRange_data"
+					:transformer="({ date }) => date"
+				/>
+				<FormSingleSelect
+					class="border dark:border-white/20 rounded p-1"
+					placeholder="This Week"
+					has-search
+					:db="dateRange_db"
+					:data="dateRange_data"
+					:transformer="({ date }) => date"
+				/>
+			</div>
+			<Tabs>
+				<TabsItem link="/dashboard" is-bordered>
+					Dashboard
+				</TabsItem>
+				<TabsItem link="/dashboard/transactions" is-bordered>
+					Transactions
+				</TabsItem>
+				<TabsItem is-bordered class="w-full text-transparent">
+					.
+				</TabsItem>
+			</Tabs>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ dashboard/components/section/dashboard/TransactionLogs.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const transactions = useTransactions()
+
+const {
+	transactions: transactionsArray,
+	flagged,
+	total,
+} = toRefs(transactions.value)
+</script>
+
+<template>
+	<SectionLogs title="Transaction Logs" />
+</template>
+
+<style scoped>
+
+</style>
